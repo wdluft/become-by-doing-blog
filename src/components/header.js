7bee7f52aeaa14ad9c94f149d35e17b320532b8f
Will Luft <william.d.luft@gmail.com>
2020-05-02 19:04:57 -0400

    Added BEM classes to header elements.

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,26 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
 import { HeaderWrapper } from './elements/headerStyles';
 
 const Header = ({ siteTitle }) => (
   <HeaderWrapper>
-    <div>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-      <p>
-        A collection of thoughts by{' '}
-        <a
-          href="https://twitter.com/iamwilldl"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Will Luft
-        </a>
-        {'. '} Can be anything from web development, sports, philosophy, or
-        whatever else grabs my attention.
-      </p>
-    </div>
+    <h1 className="header__title">
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+    <p className="header__paragraph">
+      A collection of thoughts by{' '}
+      <a
+        className="header__link"
+        href="https://twitter.com/iamwilldl"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Will Luft
+      </a>
+      {'. '} Can be anything from web development, sports, philosophy, or
+      whatever else grabs my attention.
+    </p>
   </HeaderWrapper>
 );
 
