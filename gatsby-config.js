4083e67c3215ab946d1db2b2646b04440a171f11
Will Luft <william.d.luft@gmail.com>
2020-12-19 09:10:57 -0500

    Updated color scheme

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
         name: `will-colors`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#00ffff`,
-        theme_color: `#00ffff`,
+        background_color: `hsl(209, 15%, 28%)`,
+        theme_color: `hsl(180, 94%, 58%)`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ const LayoutWrapper = styled.div`
   max-width: 768px;
   padding: 2rem;
   margin: 0 auto;
-  background: var(--black);
+  background: var(--superDarkGrey);
 
   /* Tablet */
   @media only screen and (max-width: 768px) {
